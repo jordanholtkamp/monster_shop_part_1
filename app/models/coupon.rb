195ed956897d0e70009edf77d6289aa52649f8c1
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-01-11 14:36:17 -0700

    Add relationship for orders and coupons

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,2 +1,7 @@
 class Coupon < ApplicationRecord
+    validates_presence_of :name
+    validates_presence_of :code
+    validates_presence_of :value_off
+
+    has_many :orders
 end 
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -6,4 +6,8 @@ describe Coupon, type: :model do
         it { should validate_presence_of :code }
         it { should validate_presence_of :value_off }
     end 
+
+    describe 'relationships' do 
+        it { should have_many :orders }
+    end 
 end
