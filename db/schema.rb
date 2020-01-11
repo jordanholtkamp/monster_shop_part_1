566141bd3f25d8bf7f0cb8478d5f29306e783409
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-01-11 14:07:12 -0700

    Add migration for coupon table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200106204704) do
+ActiveRecord::Schema.define(version: 20200111210405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "coupons", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.decimal "value_off"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "item_orders", force: :cascade do |t|
     t.bigint "order_id"
