0a821f88d961824d587de17baffb29452022a7ab
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-01-12 13:54:34 -0700

    Model method for no orders for a coupon

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,14 +1,18 @@
 class Coupon < ApplicationRecord
-    validates_presence_of :name
-    validates_presence_of :code
-    validates_presence_of :value_off
+  validates_presence_of :name
+  validates_presence_of :code
+  validates_presence_of :value_off
 
-    validates_uniqueness_of :name
-    validates_uniqueness_of :code
+  validates_uniqueness_of :name
+  validates_uniqueness_of :code
 
-    validates_numericality_of :value_off
-    validates_inclusion_of :value_off, :in => 1..100
+  validates_numericality_of :value_off
+  validates_inclusion_of :value_off, :in => 1..100
 
-    has_many :orders
-    belongs_to :merchant
+  has_many :orders
+  belongs_to :merchant
+
+  def no_orders?
+    orders.empty?
+  end 
 end 
