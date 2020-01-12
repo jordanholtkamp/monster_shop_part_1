403c28d91f11a20e4e5170eb15c32c72bb75b411
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-01-11 17:10:56 -0700

    Create a new coupon as a merchant

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -2,4 +2,20 @@ class Merchant::CouponsController < ApplicationController
   def index
     @merchant = current_user.merchant
   end 
+
+  def new
+  end 
+
+  def create
+    merchant = current_user.merchant
+    merchant.coupons.create(coupon_params)
+    redirect_to '/merchant/coupons'
+    flash[:success] = 'Your coupon has been created!'
+  end 
+
+  private
+  
+  def coupon_params
+    params.permit(:name, :code, :value_off)
+  end 
 end 
